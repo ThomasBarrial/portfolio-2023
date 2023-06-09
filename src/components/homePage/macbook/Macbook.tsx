3f8b14e23086ed27d64a0a49958fe5d76f9b7bd0
Thomas Barrial <thomas.barrial@gmail.com>
2023-06-09 12:20:50 +0200

    iphone issue test5

--- src/components/homePage/macbook/Macbook.tsx
+++ src/components/homePage/macbook/Macbook.tsx
@@ -5,17 +5,15 @@ import Scene from "./Scene";
 function Macbook() {
   return (
     <div className="h-screen w-screen bg-background">
-      <div style={{ width: "100%", height: "100%" }}>
-        <Canvas
-          performance={{ min: 0.1 }}
-          gl={{ antialias: false }}
-          camera={{ position: [0, 0, 23], fov: 15 }}
-        >
-          <color attach="background" args={["#080808"]} />
-          <Scene />
-          <ambientLight intensity={1} />
-        </Canvas>
-      </div>
+      <Canvas
+        performance={{ min: 0.1 }}
+        gl={{ antialias: false }}
+        camera={{ position: [0, 0, 23], fov: 15 }}
+      >
+        <color attach="background" args={["#080808"]} />
+        <Scene />
+        <ambientLight intensity={1} />
+      </Canvas>
     </div>
   );
 }
