1a5a2cf46795c9a4fa877c2faba094f32eadacbf
Thomas Barrial <thomas.barrial@gmail.com>
2023-06-11 16:18:44 +0200

    speed up aniamtions

--- src/components/homePage/selectedWork/ProjectDetails.tsx
+++ src/components/homePage/selectedWork/ProjectDetails.tsx
@@ -9,7 +9,7 @@ interface IProps {
 function ProjectDetails({ project }: IProps) {
   return (
     <div className="h-20 w-full pt-5 lg:pt-10">
-      <AnimUp duration={1.5} y={100}>
+      <AnimUp duration={1.5} y={80}>
         <div className="flex w-full flex-col items-end pb-10 font-Antonio  text-sm md:w-full  lg:flex-row">
           <div className="flex items-center lg:mr-5">
             <p className="mr-2 hidden opacity-50 md:flex">Completed</p>
