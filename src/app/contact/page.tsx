d5b3265e07fb909cd2109fca4ffe5a3d4b43482f
Thomas Barrial <thomas.barrial@gmail.com>
2024-02-13 18:04:54 -0500

    Fix last horieontal scroll issue

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,7 +13,7 @@ export default async function page() {
   const socialMedia = await clientFetch(getAllSocialMedia);
   return (
     <PageTransition>
-      <div className="relative w-screen max-w-[150rem] px-5 pt-20 lg:px-20">
+      <div className="relative w-full max-w-[150rem] px-5 pt-20 lg:px-20">
         <ContactHeader />
         <div className="flex flex-col items-start md:flex-row">
           <ContactForm />
